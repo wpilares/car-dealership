4c6740f4e46e62d946811e17e82fd19bdc49b3b4
wpilares <wgpilaresc@gmail.com>
2023-10-15 23:57:17 -0500

    added controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCardById(@Param('id') id: string) {
+    return this.cars[id];
+  }
+}
